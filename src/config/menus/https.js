64b117f0d526b3263acec1949b4a9133e9bd3d5c
yplgreenlotus <yplgreenlotus@163.com>
2020-02-13 16:42:44 +0800

    update

--- src/config/menus/https.js
+++ src/config/menus/https.js
@@ -1,1 +1,16 @@
-export default [{}]
+export default [{
+  name: 'https简介',
+  path: '/https/introduce'
+}, {
+  name: 'https 对称加密',
+  path: '/https/encryption'
+}, {
+  name: 'https 数字签名',
+  path: '/https/signature'
+}, {
+  name: 'https 连接过程',
+  path: '/https/connection'
+}, {
+  name: 'https 特点',
+  path: '/https/feature'
+}]
